889bf7f69f0620a98b494e5cbb01205652b45ec1
Carla Day <carladay44@gmail.com>
2025-09-30 23:05:55 -0400

    completing entit references, moving on to db context

---
+++ TitanHelpDesk/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TitanHelpDesk.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Ticket> Tickets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Ticket>()
+            .Property(t => t.Status)
+            .HasConversion<string>();
+
+        modelBuilder
+            .Entity<Ticket>()
+            .Property(t => t.Priority)
+            .HasConversion<string>();
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
