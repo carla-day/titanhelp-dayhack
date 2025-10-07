b5e69c531f3c715e902e3dadf4f5af0a11d73d7c
Carla Day <carladay44@gmail.com>
2025-10-06 20:27:08 -0400

    completed validations and changed up some of the styling

---
+++ TitanHelpDesk/Services/AlertService.cs
@@ -0,0 +1,12 @@
+﻿namespace TitanHelpDesk.Services
+{
+    public class AlertService
+    {
+        public event Action<string, string>? OnShow;
+
+        public void Success(string message) => OnShow?.Invoke("success", message);
+        public void Error(string message) => OnShow?.Invoke("danger", message);
+        public void Info(string message) => OnShow?.Invoke("info", message);
+        public void Warning(string message) => OnShow?.Invoke("warning", message);
+    }
+}
